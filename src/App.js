f012af840d90f8b82dd01627a3ba37dc8c755247
Michał Kozak <michalokozak@gmail.com>
2026-08-05 23:30:45 +0200

    vercel analytics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "@/App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import WeddingPage from "./pages/WeddingPage";
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
